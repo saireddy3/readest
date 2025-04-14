b88d648a05861c38260e9a22b2340fd5c46f566f
Sai Prakash Reddy <saiprakash.mandadi@skillsoft.com>
2025-04-14 09:14:48 +0530

    make it as simple standalone project

---
+++ foliatejs.d.ts
@@ -0,0 +1,21 @@
+// Declare types for @shmandadi/foliate-js package
+declare module '@shmandadi/foliate-js/epubcfi.js';
+declare module '@shmandadi/foliate-js/comic-book.js';
+declare module '@shmandadi/foliate-js/fb2.js';
+declare module '@shmandadi/foliate-js/epub.js';
+declare module '@shmandadi/foliate-js/mobi.js';
+declare module '@shmandadi/foliate-js/vendor/fflate.js';
+declare module '@shmandadi/foliate-js/overlayer.js';
+declare module '@shmandadi/foliate-js/view.js';
+declare module '@shmandadi/foliate-js/footnotes.js';
+
+// Declare duplicate types for foliate-js to handle aliased imports
+declare module 'foliate-js/epubcfi.js';
+declare module 'foliate-js/comic-book.js';
+declare module 'foliate-js/fb2.js';
+declare module 'foliate-js/epub.js';
+declare module 'foliate-js/mobi.js';
+declare module 'foliate-js/vendor/fflate.js';
+declare module 'foliate-js/overlayer.js';
+declare module 'foliate-js/view.js';
+declare module 'foliate-js/footnotes.js'; 
